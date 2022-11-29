71df84da72cffdceb13a40044a5bc03ca9b26028
lucastheldl <lucastheldl@gmail.com>
2022-11-28 21:29:20 -0300

    base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+//css
+import styles from "./App.module.css"
+
+//components
+import Footer from './components/footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main className={styles.main}><h1>Conteudo...</h1></main>
+      
+      <Footer/>
     </div>
   );
 }
